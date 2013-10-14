daf9bd7434951f4a306106884f9ba52a33031233
Arlo Belshee <arlo.belshee@live.com>
2013-10-13 19:42:46 -0700

    Test legibility improvements.

--- src/Fools.cs.Tests/CoreLanguage/MessageDelivery.cs
+++ src/Fools.cs.Tests/CoreLanguage/MessageDelivery.cs
@@ -21,48 +21,44 @@ namespace Fools.cs.Tests.CoreLanguage
 		[Test]
 		public void announce_and_notify_should_return_immediately_but_only_signal_after_all_recipients_have_completed()
 		{
-			var test_subject = new MailRoom();
-			test_subject.inform_about_message<SillyMessage>();
-			test_subject.subscribe<SillyMessage>(message_handler_that_finishes);
+			var test_subject = _create_mail_room();
+			test_subject.subscribe<SillyMessage>(async_handler);
 			test_subject.announce_and_notify_when_done(new SillyMessage("hi"), _notified.notify);
 			_notified.wait_until_count_reaches(1, Consts.TINY_DELAY)
 				.Should()
 				.BeTrue();
 			_log.Should()
-				.Equal(new object[] {"Notified: hi", "Finished: hi"});
+				.Equal(new object[] {"Started: hi", "Finished: hi"});
 		}
 
 		[Test]
 		public void announce_and_notify_should_not_fire_if_tasks_never_finish()
 		{
-			var test_subject = new MailRoom();
-			test_subject.inform_about_message<SillyMessage>();
-			test_subject.subscribe<SillyMessage>(message_handler_that_never_finishes);
+			var test_subject = _create_mail_room();
+			test_subject.subscribe<SillyMessage>(non_terminating_handler);
 			test_subject.announce_and_notify_when_done(new SillyMessage("hi"), _notified.notify);
 			_notified.wait_until_count_reaches(1, Consts.TINY_DELAY)
 				.Should()
 				.BeFalse();
 			_log.Should()
-				.Equal(new object[] {"Notified: hi"});
+				.Equal(new object[] {"Started: hi"});
 		}
 
 		[Test]
 		public void announce_should_return_after_all_recipients_have_been_notified_but_before_they_finish()
 		{
-			var test_subject = new MailRoom();
-			test_subject.inform_about_message<SillyMessage>();
-			test_subject.subscribe<SillyMessage>(message_handler_that_never_finishes);
+			var test_subject = _create_mail_room();
+			test_subject.subscribe<SillyMessage>(non_terminating_handler);
 			test_subject.announce(new SillyMessage("hi"));
 			_log.Should()
-				.Equal(new object[] {"Notified: hi"});
+				.Equal(new object[] {"Started: hi"});
 		}
 
 		[Test]
 		public void announced_messages_should_be_delivered_to_all_who_have_expressed_interest_in_that_message_type()
 		{
-			var test_subject = new MailRoom();
-			var log = store_silly_values();
-			test_subject.inform_about_message<SillyMessage>();
+			var log = store_message_values();
+			var test_subject = _create_mail_room();
 			test_subject.subscribe<SillyMessage>(log.accept);
 			test_subject.announce(new SillyMessage("1"));
 			test_subject.announce(new SillyMessage("2"));
@@ -73,9 +69,8 @@ namespace Fools.cs.Tests.CoreLanguage
 		[Test]
 		public void subscribers_should_only_get_messages_they_asked_for()
 		{
-			var test_subject = new MailRoom();
-			var log = store_silly_values();
-			test_subject.inform_about_message<SillyMessage>();
+			var log = store_message_values();
+			var test_subject = _create_mail_room();
 			test_subject.subscribe<SillyMessage>(log.accept);
 			test_subject.announce(new SillyMessage("silly"));
 			test_subject.announce(new SeriousMessage("serious"));
@@ -87,8 +82,7 @@ namespace Fools.cs.Tests.CoreLanguage
 		public void
 			subscribing_for_a_message_while_processing_that_message_should_result_in_new_subscriber_not_receiving_message()
 		{
-			var test_subject = new MailRoom();
-			test_subject.inform_about_message<SillyMessage>();
+			var test_subject = _create_mail_room();
 			test_subject.subscribe<SillyMessage>(
 				new RecursiveSubscriber(3, test_subject, _log).subscribe_recursively_until_counter_expires);
 			test_subject.subscribe<SillyMessage>(
@@ -108,33 +102,34 @@ namespace Fools.cs.Tests.CoreLanguage
 			_log = new List<string>();
 		}
 
-		private void message_handler_that_finishes([NotNull] SillyMessage m, [NotNull] Action done)
+		[NotNull]
+		private static MailRoom _create_mail_room()
 		{
-			_log.Add(string.Format("Notified: {0}", m.value));
+			var test_subject = new MailRoom();
+			test_subject.inform_about_message<SillyMessage>();
+			test_subject.inform_about_message<SeriousMessage>();
+			return test_subject;
+		}
+
+		private void async_handler([NotNull] SillyMessage m, [NotNull] Action done)
+		{
+			_log.Add(string.Format("Started: {0}", m.value));
 			new Task(() => {
 				_log.Add(string.Format("Finished: {0}", m.value));
 				done();
 			}).Start();
 		}
 
-		private void message_handler_that_never_finishes([NotNull] SillyMessage m, [NotNull] Action done)
+		private void non_terminating_handler([NotNull] SillyMessage m, [NotNull] Action done)
 		{
-			_log.Add(string.Format("Notified: {0}", m.value));
+			_log.Add(string.Format("Started: {0}", m.value));
 		}
 
 		[NotNull]
-		private static MessageLog<string, SillyMessage> store_silly_values()
+		private static MessageLog<string, SillyMessage> store_message_values()
 		{
 			// ReSharper disable PossibleNullReferenceException
 			return new MessageLog<string, SillyMessage>(m => m.value);
-			// ReSharper restore PossibleNullReferenceException
-		}
-
-		[NotNull]
-		private static MessageLog<string, MailMessage> store_all_values()
-		{
-			// ReSharper disable PossibleNullReferenceException
-			return new MessageLog<string, MailMessage>(m => ((SillyMessage) m).value);
 			// ReSharper restore PossibleNullReferenceException
 		}
 
